e5820f282100c9b4bc44728e9bb5a3eab87b372a
DiegoFleitas <diego.fleitas68@gmail.com>
2019-02-18 23:01:53 -0300

    fixed random posting

--- src/Bot/mixfakepost.php
+++ src/Bot/mixfakepost.php
@@ -30,18 +30,19 @@ $bot_pool = [
 //TODO CommentBot2005 -- it's Max's bot
 //TODO ClickBot 2000
 
-//$bot = $bot_pool[array_rand($bot_pool)];
+$bot = $bot_pool[array_rand($bot_pool)];
 //$bot = 'StyletransferBot9683';
 //$bot = 'ArtPostBot 1519';
-$bot = 'Botob 8008';
+//$bot = 'Botob 8008';
 //$bot = 'InspiroBot Quotes';
 //$bot = 'CensorBot 1111';
 //$bot = 'EmojiBot 101';
 //$bot = 'CountryBot 0208';
 // remove it so it doesn't gets chosen again
-//unset($bot_pool[$bot]);
-//$bot2 = $bot_pool[array_rand($bot_pool)];
-$bot2 = 'StyletransferBot9683';
+$key = array_search($bot, $bot_pool);
+unset($bot_pool[$key]);
+$bot2 = $bot_pool[array_rand($bot_pool)];
+//$bot2 = 'StyletransferBot9683';
 //$bot2 = 'ArtPostBot 1519';
 //$bot2 = 'Botob 8008';
 //$bot2 = 'InspiroBot Quotes';
