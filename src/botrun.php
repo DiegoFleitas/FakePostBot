194eb43417ff29df870764f31e5679d7017d6323
DiegoFleitas <diego.fleitas68@gmail.com>
2019-03-01 00:20:56 -0300

    Done scheduled posting

--- src/botrun.php
+++ src/botrun.php
@@ -20,32 +20,10 @@ $bot_pool = [
     'US Election Bot 1776'
 ];
 
-//TODO US Election Bot 1776
-//TODO ToolpostBot -- thank admin
-//TODO Millennialbot 2019
-//TODO CommentBot2005 -- it's Max's bot
-//TODO ClickBot 2000
-
 $bot = $bot_pool[array_rand($bot_pool)];
-//$bot = 'US Election Bot 1776';
-//$bot = 'StyletransferBot9683';
-//$bot = 'ArtPostBot 1519';
-//$bot = 'Botob 8008';
-//$bot = 'InspiroBot Quotes';
-//$bot = 'CensorBot 1111';
-//$bot = 'EmojiBot 101';
-//$bot = 'CountryBot 0208';
-// remove it so it doesn't gets chosen again
 $key = array_search($bot, $bot_pool);
 unset($bot_pool[$key]);
 $bot2 = $bot_pool[array_rand($bot_pool)];
-//$bot2 = 'StyletransferBot9683';
-//$bot2 = 'ArtPostBot 1519';
-//$bot2 = 'Botob 8008';
-//$bot2 = 'InspiroBot Quotes';
-//$bot2 = 'CensorBot 1111';
-//$bot2 = 'EmojiBot 101';
-//$bot2 = 'CountryBot 0208';
 
 
 $dt = new FakepostBot\DataLogger();
